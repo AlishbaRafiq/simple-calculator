88be0724cba74af4bafb7a024b66c5ad66b94abb
AlishbaRafiq <ar2313077@gmail.com>
2024-04-02 10:25:56 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// printing a welcome message
+console.log("\n\tWelcome to 'alishbarafiq' - CLI Simple Calculator\n");
+
+// Asking questions from users through the inquirer
+
+let answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// conditional statement
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Invalid operator");
+}
